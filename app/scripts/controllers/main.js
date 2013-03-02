8638f39eee7e086eb742f08b9ffa01fe1c98fb23
Andreas Krimbacher <andreas.krimbacher@gmail.com>
2013-03-02 20:42:20 +0100

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('swaApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Testacular'
+    ];
+
+
+
+
+  });
