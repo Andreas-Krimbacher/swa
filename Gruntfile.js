d854636a60cee826c3a95304a2298f76aa61aef2
Andreas Krimbacher <andreas.krimbacher@gmail.com>
2013-03-03 18:50:52 +0100

    slider directive

--- Gruntfile.js
+++ Gruntfile.js
@@ -113,7 +113,7 @@ module.exports = function (grunt) {
       options: {
         sassDir: '<%= yeoman.app %>/styles',
         cssDir: '.tmp/styles',
-        imagesDir: '<%= yeoman.app %>/images',
+        imagesDir: '<%= yeoman.app %>/styles/images',
         javascriptsDir: '<%= yeoman.app %>/scripts',
         fontsDir: '<%= yeoman.app %>/styles/fonts',
         importPath: '<%= yeoman.app %>/components',
